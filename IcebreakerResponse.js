86e98415a081de658949e8d95e896a51cda8f625
Yidan Wang <wangyidan621@gmail.com>
2017-11-16 00:54:15 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(data){
+  return data.map(function(item){
+    return new IcebreakerResponse(item)
+  })
+}
